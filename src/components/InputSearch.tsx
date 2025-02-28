17ba7878670a1f3c8ab9b746ff8fb4fccfaf956c
NatalioDev <nataliodg.dev@gmail.com>
2025-02-27 22:22:09 -0300

    changes placeholder

--- src/components/InputSearch.tsx
+++ src/components/InputSearch.tsx
@@ -21,7 +21,7 @@ const InputSearch = () => {
                 type="text" 
                 name="search" 
                 value={searchParam}
-                placeholder="Ej: apples,flour,sugar"
+                placeholder="Ej: apples"
                 onChange={(e) => setSearchParam(e.target.value)}
                 className="bg-slate-200 text-teal-950 font-bold uppercase p-3 px-4 lg:px-8 rounded-3xl outline-none shadow-md shadow-black focus:shadow-teal-500 w-[150px] text-sm
                 sm:w-52
